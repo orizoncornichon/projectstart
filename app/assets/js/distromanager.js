6dedc0f17fff1702e105ed7354963740439e6ef5
Nefers12 <steoxys42@gmail.com>
2025-07-23 21:21:46 +0200

    Update REMOTE_DISTRO_URL and increment version to 1.0.4

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -3,7 +3,7 @@ const { DistributionAPI } = require('helios-core/common')
 const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
-exports.REMOTE_DISTRO_URL = 'http://orizonx.cluster021.hosting.ovh.net/orizon.json'
+exports.REMOTE_DISTRO_URL = 'http://fastdl.terrymarquis.fr:82/orizon/orizon.json'
 // exports.REMOTE_DISTRO_URL = ''
 
 const api = new DistributionAPI(
